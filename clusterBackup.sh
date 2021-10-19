1d67945e178e384fcbedcd967a3889ff0cbfc57c
root <root@azlvicntrldb04.h3p4cpim2ztefm51whrqkfyd0e.ax.internal.cloudapp.net>
2021-10-19 17:06:41 +0000

    Dumpall comprobando argumentos de entrada

--- clusterBackup.sh
+++ clusterBackup.sh
@@ -4,15 +4,31 @@
 # The second arg ($2) is the database to backup and is included in the file name.
 # $(date +"%Y_%m_%d") includes the current system date into the actual file name.
 
-echo -e "\nRealizando backup de $1..."
-today=`date '+%Y_%m_%d__%H_%M_%S'`;
-nombreBackup="backup_postgresql_$1_`date '+%Y-%m-%d_%H:%M:%S'`.sql"
-echo -e '\nTamaño backup anterior:' 
-var=$(find ~/Cluster_Dumps/ -type f -mtime -5 | tail -1)
-du -sh $var
-echo $backupAnterior
-#psql -U $1 -d $2 -c "SELECT pg_database.datname, pg_size_pretty(pg_database_size(pg_database.datname)) AS SIZE FROM pg_database WHERE pg_database.datname='$2';"
-pg_dumpall -U $1 > ~/Cluster_Dumps/$nombreBackup
-echo -e '\nTamaño nuevo backup:'
-du -sh  ~/Cluster_Dumps/$nombreBackup
-echo ''
+
+if [ -n "$1" ]; then # If first parameter passed
+
+	echo -e "\nRealizando backup de $1..."
+	today=`date '+%Y_%m_%d__%H_%M_%S'`;
+	nombreBackup="backup_postgresql_$1_`date '+%Y-%m-%d_%H:%M:%S'`.sql"
+
+	if [ "$(ls $~/backups)" ]
+	then
+	
+		echo -e '\nTamaño backup anterior:'
+		ultimoBackup=$(find ~/backups/ -type f -mtime -5 | tail -1)
+		du -sh $ultimoBackup
+		#psql -U $1 -d $2 -c "SELECT pg_database.datname, pg_size_pretty(pg_database_size(pg_database.datname)) AS SIZE FROM pg_database WHERE pg_database.datname='$2';"
+	else
+		echo "Este es el primer backup de esta base de datos"
+	fi
+
+	pg_dumpall -U $1 > ~/backups/$nombreBackup
+	echo -e '\nTamaño nuevo backup:'
+	du -sh  ~/backups/$nombreBackup
+	echo ''
+
+else
+
+	echo -e '\nSe debe especificar la BBDD\n'
+
+fi
